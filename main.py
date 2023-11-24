ab16b778d917faf0325285b95d915f73012982f2
Mikael Fernandes Moreira <mikaelfernandesmoreira@gmail.com>
2023-11-24 11:34:20 -0300

    ajuste parametro max metodo /quadrados como opcional

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
+from typing import Optional
 
 app = FastAPI()
 
 
 @app.get("/quadrados")
-def quadrados(max : int):
+def quadrados(max : Optional[int] = 5):
    quadrados = []
    for i in range(1, max+1):
        quadrados.append(i**2)
