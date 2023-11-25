a49ee6767c3b022032d4c2fa84fb564750d7791e
Mikael Fernandes Moreira <mikaelfernandesmoreira@gmail.com>
2023-11-25 13:39:43 -0300

    adição de validações

--- routes.py
+++ routes.py
@@ -6,17 +6,32 @@ from app import app
 
 @app.get("/quadrados")
 def quadrados(max : Optional[int] = 5):
+   if not isinstance(max, int):
+      return "O valor de 'max' deve ser um número inteiro."
+   if max <= 0:
+      return "Por Favor, informe um número 'max' maior que 0."
+
    quadrados = []
    for i in range(1, max+1):
        quadrados.append(i**2)
   
-   return {
+   return { 
        "max" : max,
        "quadrados" : quadrados
    }
 
+
 @app.get("/tabuada/{num}")
 def tabuada(num : int, start : Optional[int] = 1, end : Optional[int] = 10):
+   if not isinstance(num, int):
+      return "O valor de 'num' deve ser um número inteiro."
+   if not isinstance(start, int):
+      return "O valor de 'start' deve ser um número inteiro."
+   if not isinstance(end, int):
+      return "O valor de 'end' deve ser um número inteiro."
+   if start > end:
+      return "O número 'start' não pode ser maior que o 'end'"
+
    tabuada = []
    for i in range(start, end+1):
        tabuada.append(num*i)
@@ -30,8 +45,12 @@ def tabuada(num : int, start : Optional[int] = 1, end : Optional[int] = 10):
 
 @app.post("/bhaskara")
 def bhaskara(equacao : Bhaskara):
+   if not isinstance(equacao, Bhaskara):
+      return "O valor de 'equacao' deve ter valores válidos"
+   if equacao.a == 0:
+      return "O coeficiente 'a' não pode ser igual a 0."
+
    x1, x2 = equacao.getResult()
-   print(x1)
    return {
        "eq" : equacao.getEquacao(),
        "x1" : x1.real,
@@ -40,6 +59,9 @@ def bhaskara(equacao : Bhaskara):
 
 @app.post("/conta")
 def conta(frase : Frase):
+   if not isinstance(frase, Frase):
+      return "O valor de 'frase' deve ser uma frase válida"
+
    vogais, consoantes, espacos, outros = frase.count()
    return {
        "frase": frase.frase,
